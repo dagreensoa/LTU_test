e43644256a494abf0ae1567812986db69788dc4d
LIn Kuan Ting <dagreensoa@gmail.com>
2024-08-23 10:35:12 +0800

    第一次測試

---
+++ L123/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace L123
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("");
+        }
+    }
+}
